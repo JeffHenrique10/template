b988b71393e6f657a4143d22025908824182a6ee
JeffHenrique10 <jeffhenrique91@gmail.com>
2019-05-29 17:49:35 -0300

    template

---
+++ src/pages/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+import styles from './styles';
+
+const Main = () => (
+  <View style={styles.container}>
+    <Icon name="react" size={150} color="#FFF" />
+  </View>
+);
+
+export default Main;
